1ed36685ad18ad06ffea1a958636394563cfe43d
iulian <iulian.titei@gail.com>
2019-10-30 10:56:40 +0200

    v1

---
+++ music_reports.py
@@ -0,0 +1,25 @@
+# importe file
+
+
+def import_music(filename="text_albums_data.txt"):
+    try:
+        with open(filename, "r") as f:
+            album_list = []
+            for line in f:
+                album_list.append(line)
+            album_list = [x.strip().split(',') for x in album_list]
+            return album_list
+    except FileNotFoundError:
+        print('File not found !')
+
+# As a user I want to view all imported albums
+
+
+def display_albums():
+    x = import_music()
+    albums = [item[1] for item in x]
+    for i, data in enumerate(albums):
+        print(i+1, data)
+
+
+display_albums()
