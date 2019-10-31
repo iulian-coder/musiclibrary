018d1607e27f478f5e2800b645614120a1ff6471
iulian-coder <iulian.titei@gmail.com>
2019-10-31 15:38:39 +0200

    v6

--- display.py
+++ display.py
@@ -10,46 +10,36 @@ def import_music(filename="text_albums_data.txt"):
     except FileNotFoundError:
         print('File not found !')
 
-# As a user I want to view all imported albums
 
 def display_albums():
+    print("All Albums\n")
     x = import_music()
     albums = [item[1] for item in x]
     for i, data in enumerate(albums):
         print(i+1, data)
 
 
-
-
-
-    
 def search_artist():
+    user_input = input("Write artist ")
     lista = import_music()
     for x in lista:
-        if x[0] == "Pink Floyd":
+        if x[0] == user_input:
             print('Artisit: {}\nAlbum: {}\nRelease Year: {}\nGenre: {}\nDuration {}\n'.format(x[0], x[1], x[2], x[3], x[4]))
 
-
-
-# As a user I want to find album by album name
-
-def search_alb64utum():
-    lista = import_music()
-    for x in lista:
-        if x[1] == "Boston":
-            print('Artisit: {}\nAlbum: {}\nRelease Year: {}\nGenre: {}\nDuration {}\n'.format(x[0], x[1], x[2], x[3], x[4]))
-        
-
-
-
-# As a user I want to find album by album name
 
 def search_album():
-    nume = input('What album are you looking for? ')
+    user_input = input('Write album ')
     lista = import_music()
     for x in lista:
-        if x[1] == nume:
+        if x[1] == user_input:
             print('Artisit: {}\nAlbum: {}\nRelease Year: {}\nGenre: {}\nDuration {}\n'.format(x[0], x[1], x[2], x[3], x[4]))
 
-       
+
+def search_genre():
+    user_input = input('Write genre ')
+    list = import_music()
+    for x in list:
+        if x[3] == user_input:
+            print('Artist: {}\nAlbum: {}\nRelease Year: {}\nGenre: {}\nDuration: {}\n'.format(x[0], x[1], x[2], x[3], x[4]))
+
 
--- music_reports.py
+++ music_reports.py
@@ -1,28 +1,16 @@
+from display import *
 import os
-from display import search_album
 
-os.system("clear")
-'''
-def import_music(filename="text_albums_data.txt"):
-    try:
-        with open(filename, "r") as f:
-            album_list = []
-            for line in f:
-                album_list.append(line)
-            album_list = [x.strip().split(',') for x in album_list]
-            return album_list
-    except FileNotFoundError:
-        print('File not found !')
-'''
+
 
 def meniu():
     while True:
-        print(  """\t\t MENIU
+        print("""\t\t MENIU
         
         1. View all imported albums
         2. Find all albums by genre
-        3. Find all albums from given time range
-        4. Find shortest/longest album
+        3. Find all albums from given time range (Under Construction)
+        4. Find shortest/longest album (Under Construction)
         5. Find all albums created by given artist
         6. Find album by album name
         7. Full Report
@@ -30,23 +18,35 @@ def meniu():
 
         \t\t SPECIAL
 
-        9. View similar music by genres
-       10. Add new album
-       11. Edit album
-       12. Export album to external file""")
+        9. View similar music by genres (Under Construction)
+       10. Add new album (Under Construction)
+       11. Edit album (Under Construction)
+       12. Export album to external file - Under Construction """)
 
         user_input = input("\nWhat would you like to do? ")
-        if user_input =="1": 
-            search_album() 
-        elif user_input =="2":
-            print("\n Ceva Deleted") 
-        elif user_input =="3":
-            print("\n Ceva Record Found") 
+        if user_input == "1":
+            display_albums()
+            input("Press enter to continue...")
+            os.system("clear")
+        elif user_input == "2":
+            search_genre()
+            input("Press enter to continue...")
+            os.system("clear")
+        elif user_input == "3":
+            print("\n Under Construction")
+        elif user_input =="4":
+            print("\n Under Construction")
+        elif user_input =="5":
+            print("\n Ceva Record Found")
+        elif user_input =="6":
+            print("\n Ceva Record Found")
+        elif user_input =="7":
+            print("\n Ceva Record Found")    
         elif user_input =="8":
             print("\n Goodbye")
             break 
         elif user_input !="":
-            print("\n Not Valid Choice Try again") 
+            print("\n Not Valid Choice Try again")            
 
 
 meniu()
