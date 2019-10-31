f660b1a2761ef55dd434405a617cf9fc2de6e250
iulian-coder <iulian.titei@gmail.com>
2019-10-31 13:10:19 +0200

    v2

--- display.py
+++ display.py
@@ -10,6 +10,7 @@ def import_music(filename="text_albums_data.txt"):
     except FileNotFoundError:
         print('File not found !')
 
+# As a user I want to view all imported albums
 
 def display_albums():
     x = import_music()
@@ -18,3 +19,42 @@ def display_albums():
         print(i+1, data)
 
 
+display_albums()
+# As a user I want to find all albums created by given artist
+print("\n**********")
+
+
+    
+def search_artist():
+    lista = import_music()
+    for x in lista:
+        if x[0] == "Pink Floyd":
+            print('Artisit: {}\nAlbum: {}\nRelease Year: {}\nGenre: {}\nDuration {}\n'.format(x[0], x[1], x[2], x[3], x[4]))
+
+
+search_artist()
+
+# As a user I want to find album by album name
+
+def search_album():
+    lista = import_music()
+    for x in lista:
+        if x[1] == "Boston":
+            print('Artisit: {}\nAlbum: {}\nRelease Year: {}\nGenre: {}\nDuration {}\n'.format(x[0], x[1], x[2], x[3], x[4]))
+        
+search_album()
+
+
+# As a user I want to find album by album name
+
+def search_album():
+    nume = input('SCrie albumu')
+    lista = import_music()
+    for x in lista:
+        if x[1] == nume:
+            print('Artisit: {}\nAlbum: {}\nRelease Year: {}\nGenre: {}\nDuration {}\n'.format(x[0], x[1], x[2], x[3], x[4]))
+
+       
+search_album()
+
+print(import_music())
--- music_reports.py
+++ music_reports.py
@@ -14,20 +14,3 @@ def import_music(filename="text_albums_data.txt"):
     except FileNotFoundError:
         print('File not found !')
 
-# As a user I want to find all albums created by given artist
-print("Merge!")
-
-
-artist = "Pink Floyd" #n = input('Search all albums by given artist')
-
-    
-def search_artist():
-    lista = import_music()
-    for x in lista:
-        if x[0] == "Pink Floyd":
-            print('Artisit: {}\nAlbum: {}\nRelease Year: {}\nGenre: {}\nDuration {}\n======================='.format(x[0], x[1], x[2], x[3], x[4]))
-
-
-
-search_artist()
-
